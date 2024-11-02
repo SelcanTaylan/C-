319c99856fbf45240507c0589a72d7843d992102
Selcan <selcantaylan@gmail.com>
2024-11-02 11:31:31 +0300

    Add files via upload

---
+++ örnek2/örnek2/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace örnek2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSepeteEkle = new System.Windows.Forms.Button();
+            this.txtSepeteEkle = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSepeteEkle
+            // 
+            this.btnSepeteEkle.BackColor = System.Drawing.Color.White;
+            this.btnSepeteEkle.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSepeteEkle.Location = new System.Drawing.Point(24, 212);
+            this.btnSepeteEkle.Name = "btnSepeteEkle";
+            this.btnSepeteEkle.Size = new System.Drawing.Size(221, 42);
+            this.btnSepeteEkle.TabIndex = 0;
+            this.btnSepeteEkle.Text = "Sepete Ekle";
+            this.btnSepeteEkle.UseVisualStyleBackColor = false;
+            this.btnSepeteEkle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSepeteEkle
+            // 
+            this.txtSepeteEkle.BackColor = System.Drawing.Color.White;
+            this.txtSepeteEkle.Location = new System.Drawing.Point(12, 169);
+            this.txtSepeteEkle.Name = "txtSepeteEkle";
+            this.txtSepeteEkle.Size = new System.Drawing.Size(233, 22);
+            this.txtSepeteEkle.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(279, 73);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(182, 244);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(528, 408);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtSepeteEkle);
+            this.Controls.Add(this.btnSepeteEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSepeteEkle;
+        private System.Windows.Forms.TextBox txtSepeteEkle;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
